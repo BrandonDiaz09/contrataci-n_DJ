ee03a4fcad5bbf49f9a1fe43d582e471a566f8aa
Brandon Diaz <branal845diaz@gmail.com>
2023-06-17 23:01:36 -0600

    Agregué la configuración para la base de datos y el cript base para php

---
+++ php/scriptBase.php
@@ -0,0 +1,8 @@
+<?php
+require 'conexionBD.php';
+// Aquí puedes realizar tus consultas o interacciones con la base de datos utilizando la conexión establecida
+
+
+// Cierra la conexión
+$conexion->close();
+?>
