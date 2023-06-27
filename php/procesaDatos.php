5528361aeb7380122267f45e344d27f0742ae729
Brandon Diaz <branal845diaz@gmail.com>
2023-06-27 08:02:30 -0600

    Se modifico el admin para borrar, crear, leer y editar, solo funciona bien borrar y leer

--- php/procesaDatos.php
+++ php/procesaDatos.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
   <link rel="stylesheet" href="../Styles/global.css">
   <link rel="stylesheet" type="text/css" href="../styles/styles_form.css">
-  <script src="./JavaScript/form.js"></script>
+  <script src="../JavaScript/form.js"></script>
 </head>
 
 <body>
